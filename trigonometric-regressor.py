2c38626a3589c6e3cff597dcd3b268e748d0ab19
Dag Erlandsen <dag@spinningowl.com>
2018-04-28 12:20:32 +0700

    Make terminology more consistent and tweak constants.

--- trigonometric-regressor.py
+++ trigonometric-regressor.py
@@ -15,6 +15,10 @@ import numpy as np
 import math
 import time
 
+PI = 3.14159265359
+TRAINING_RATIO = 0.8   # This much of the dataset will be used for training, the rest for evaluation
+DATASET_SIZE = 100
+
 class ConfigPermutation:
     def __init__(self):
         SIZES = [0, 8, 256]
@@ -40,16 +44,12 @@ def create_dataset(size):
     output = np.array([math.sin(a*PI*2)+math.cos((a+0.2)*PI*4) for a in inputs])    # Trigonometric equation
     return inputs, output
 
-PI = 3.14159265359
-TRAINING_RATIO = 0.75   # This much of the dataset will be used for training
-DATASET_SIZE = 100
-
 inputs, output = create_dataset(DATASET_SIZE)
 bound_index = int(TRAINING_RATIO*len(inputs))
 inputs_training = inputs[:bound_index]
-inputs_verification = inputs[bound_index:]
+inputs_evaluation = inputs[bound_index:]
 output_training = output[:bound_index]
-output_verification = output[bound_index:]
+output_evaluation = output[bound_index:]
 
 column =  tf.feature_column.numeric_column('x')
 
@@ -72,22 +72,22 @@ while not configs.is_empty():
     train_spec = tf.estimator.TrainSpec(input_fn=train_input, max_steps=2000)
 
     eval_input = tf.estimator.inputs.numpy_input_fn(
-        x={"x": inputs_verification},
-        y=output_verification, 
+        x={"x": inputs_evaluation},
+        y=output_evaluation, 
         shuffle=True,
-        batch_size=len(inputs_verification))
+        batch_size=len(inputs_evaluation))
     eval_spec = tf.estimator.EvalSpec(input_fn=eval_input, steps=100)
     
     tf.estimator.train_and_evaluate(regressor, train_spec, eval_spec)
 
     predict_input = tf.estimator.inputs.numpy_input_fn(
-         x={"x": inputs_verification},
+         x={"x": inputs_evaluation},
          num_epochs=1, shuffle=False)
     results = list(regressor.predict(predict_input))
 
     average_error = 0   
     for i in range(len(results)):
-         average_error += abs(results[i]["predictions"]-output_verification[i])     # Could use RMSE or similar, but this is more intuitive
+         average_error += abs(results[i]["predictions"]-output_evaluation[i])     # Could use RMSE or similar, but this is more intuitive
     average_error /= len(results)
 
     delta_time = time.time() - start_time;
